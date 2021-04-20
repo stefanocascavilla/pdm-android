8aa9193f62e1502dfe79fe01b3151cdaa946718d
Stefano Cascavilla <cascavilla1@gmail.com>
2021-04-20 15:07:55 +0200

    Created new FriendsListActivity

---
+++ app/src/main/java/com/example/myfriendshouse/FriendsListActivity.kt
@@ -0,0 +1,11 @@
+package com.example.myfriendshouse
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FriendsListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends_list)
+    }
+}
